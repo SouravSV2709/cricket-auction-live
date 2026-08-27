127c12f21a13719d9ff541214341db0c85ba93fa
Sourav Mukherjee <souravos27@gmail.com>
2026-08-27 12:17:07 +0530

    Change the unsold gif

--- src/components/SpectatorLiveDisplay2.jsx
+++ src/components/SpectatorLiveDisplay2.jsx
@@ -591,11 +591,11 @@ const getRandomSoldAudio = () => {
 };
 
 const unsoldMedia = [
-    '/sounds/unsold1207.gif',
-    '/sounds/unsold3.gif',
-    '/sounds/unsold5.gif',
-    '/sounds/unsold6.gif',
-    '/sounds/unsold7.gif'
+    '/sounds/UNSOLD2708.gif'
+    // '/sounds/unsold3.gif',
+    // '/sounds/unsold5.gif',
+    // '/sounds/unsold6.gif',
+    // '/sounds/unsold7.gif'
 ];
 
 const unsoldAudio = new Audio('/sounds/unsold4.mp3');
